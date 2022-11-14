2d602daed76c41c4cfc3af2ddea0ddf6707f27c3
Clarissa Joy Flores <cjgflores@csmbataan.ph.education>
2022-11-14 11:17:56 +0800

    Date Caluculator

---
+++ joy.PrelimActivity1/joy.Datecalculator/Pages/Dates.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace joy.Datecalculator.Pages
+{
+    public class DatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
